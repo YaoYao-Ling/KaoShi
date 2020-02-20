c32e60a423f5fc3206f900f2d9fdd71f53b5fd0f
二蛋 <51009841+2472901304@users.noreply.github.com>
2020-02-20 18:36:13 +0800

    完成添加面试

--- sign/src/reducers/sign.ts
+++ sign/src/reducers/sign.ts
@@ -1,5 +1,6 @@
 const INITIAL_STATE = {
-    address: {}
+    address: {},
+    flag: -1     //-1表示未提交表单，0表示提交失败，1表示提交成功
 }
 
 export default function sign(state = INITIAL_STATE,action){
@@ -8,6 +9,11 @@ export default function sign(state = INITIAL_STATE,action){
             return {
                 ...state,
                 address: action.payload
+            }
+        case 'SUBMIT_SIGN':
+            return {
+                ...state,
+                flag: action.payload
             }
         default:
             return state
